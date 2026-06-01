0a38ff0a231c755f3e11aa118d52bab3693355ad
mounikaburri <mounikaburri21@gmail.com>
2026-06-01 18:09:46 +0530

    add login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
       setError("");
 
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://e-cart-backend-39er.onrender.com/api/auth/login",
         {
           email,
           password,
